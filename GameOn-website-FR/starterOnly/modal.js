3809c0b185063aa5800c155aef3f4492f913ea0f
Quadjovie Antonio <quadjovie.antonio@gmail.com>
2022-09-23 22:15:18 +0200

    factorisation

--- GameOn-website-FR/starterOnly/modal.js
+++ GameOn-website-FR/starterOnly/modal.js
@@ -30,47 +30,45 @@ function close() {
 }
 
 // Formulaire dom element
-const form = document.getElementById ('form');
-const firstname = document.getElementById ('firstname');
-const lastname = document.getElementById ('lastname');
-const mail = document.getElementById ('yourMail');
-const birth = document.getElementById ('birthday');
-const tournament = document.getElementById('howManyTournament');
-const location1 = document.getElementById("location1");
-const location2 =  document.getElementById("location2");
-const location3 =  document.getElementById("location3");
-const location4 =  document.getElementById("location4");
-const location5 =  document.getElementById("location5");
-const location6 =  document.getElementById("location6");
-const checkbox1CGU = document.getElementById("checkbox1"); 
+const form = document.getElementById("form");
+const firstname = document.getElementById("firstname");
+const lastname = document.getElementById("lastname");
+const mail = document.getElementById("yourMail");
+const birth = document.getElementById("birthday");
+const tournament = document.getElementById("howManyTournament");
+const locations = document.getElementsByName("location");
+
+const checkbox1CGU = document.getElementById("checkbox1");
 // Regex validation
 const numbers = /^[0-9]+$/;
-const mailRegex = /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+const mailRegex =
+  /^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
 const birthdayRegex = /^\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])$/;
 const nameRegex = /(.*[a-z]){2}/i;
 // error validation dom element
 
-const errorFirst = document.getElementById ('error-first');
-const errorLast = document.getElementById ('error-last');
-const errorMail = document.getElementById ('error-mail');
-const errorBirth = document.getElementById ('error-birth');
-const errorQuantity = document.getElementById ('error-howMany');
-const errorCity = document.getElementById ('error-city');
-const errorValidation = document.getElementById ('error-validation');
+const errorFirst = document.getElementById("error-first");
+const errorLast = document.getElementById("error-last");
+const errorMail = document.getElementById("error-mail");
+const errorBirth = document.getElementById("error-birth");
+const errorQuantity = document.getElementById("error-howMany");
+const errorCity = document.getElementById("error-city");
+const errorValidation = document.getElementById("error-validation");
 
 // messages de validation
 
-const confirmation = document.getElementById ('confirmation');
-const confirmationCloseBtn = document.getElementsByClassName('btn-close');
+const confirmation = document.getElementById("confirmation");
+const confirmationCloseBtn = document.getElementsByClassName("btn-close");
 confirmationCloseBtn[0].addEventListener("click", close);
 
-// functions validation formulaire 
+// functions validation formulaire
 
-form.addEventListener('submit', (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
-})
+  validate();
+});
 
-function validate () {
+function validate() {
   let firstChecked;
   let lastChecked;
   let mailChecked;
@@ -79,92 +77,92 @@ function validate () {
   let radioChecked;
   let CGUChecked;
 
-  if (!firstname.value.match(nameRegex) || firstname.value == ' ' || firstname.value.length < 2) {
-    errorFirst.innerText = 'Veuillez renseigner un prénom valide';
-    errorFirst.style.color = '#FF4E60';
-    errorFirst.style.fontSize = '0.8rem';
-    errorFirst.style.marginTop = '10px';
-    firstname.style.border = 'solid #FF4E60 2px';
+  if (
+    !firstname.value.match(nameRegex) ||
+    firstname.value == " " ||
+    firstname.value.length < 2
+  ) {
+    errorFirst.style.display = "block";
+    firstname.style.border = "solid #FF4E60 2px";
   } else {
-    errorFirst.style.display = 'none';
-    firstname.style.border = 'none';
+    errorFirst.style.display = "none";
+    firstname.style.border = "none";
     firstChecked = true;
-  };
-
-  if (!lastname.value.match(nameRegex) || lastname.value == ' ' || lastname.value == null || lastname.value.length < 2) { 
-    errorLast.innerText = 'Veuillez renseigner un nom valide';
-    errorLast.style.color = '#FF4E60';
-    errorLast.style.fontSize = '0.8rem';
-    errorLast.style.marginTop = '10px';
-    lastname.style.border = 'solid #FF4E60 2px';      
-}  else {
-  errorLast.style.display = 'none';
-  lastname.style.border = 'none';
-  lastChecked = true;
-};
-
-if (!mailRegex.test(mail.value)) { 
-  errorMail.innerText = 'Veuillez renseigner une adresse mail valide';
-  errorMail.style.color = '#FF4E60';
-  errorMail.style.fontSize = '0.8rem';
-  errorMail.style.marginTop = '10px';
-  mail.style.border = 'solid #FF4E60 2px';
-} else {
-errorMail.style.display = 'none';
-mail.style.border = 'none';
-mailChecked = true;
-};
-
-if (!birth.value.match(birthdayRegex)) { 
-  errorBirth.innerText = 'Veuillez remplir votre date danniversaire';
-  errorBirth.style.color = '#FF4E60';
-  errorBirth.style.fontSize = '0.8rem';
-  errorBirth.style.marginTop = '10px';
-  birth.style.border = 'solid #FF4E60 2px';
-  } else {
-  errorBirth.style.display = 'none';
-  birth.style.border = 'none';
-  birthChecked = true;      
   }
 
-  if (!tournament.value.match(numbers)) { 
-    errorQuantity.innerText = 'Vous devez indiquer un nombre';
-    errorQuantity.style.color = '#FF4E60';
-    errorQuantity.style.fontSize = '0.8rem';
-    errorQuantity.style.marginTop = '10px';
-    tournament.style.border = 'solid #FF4E60 2px';
+  if (
+    !lastname.value.match(nameRegex) ||
+    lastname.value == " " ||
+    lastname.value == null ||
+    lastname.value.length < 2
+  ) {
+    errorLast.style.display = "block";
+    lastname.style.border = "solid #FF4E60 2px";
   } else {
-    errorQuantity.style.display = 'none';
-    tournament.style.border = 'none';
-    tournamentChecked = true;
-  };
+    errorLast.style.display = "none";
+    lastname.style.border = "none";
+    lastChecked = true;
+  }
 
-  if (!location1.checked && !location2.checked && !location3.checked && !location4.checked && !location5.checked && !location6.checked) { 
-    errorCity.innerText = 'choisissez une ville';
-    errorCity.style.color = '#FF4E60';
-    errorCity.style.fontSize = '0.8rem';
-    errorCity.style.marginTop = '10px';          
+  if (!mailRegex.test(mail.value)) {
+    errorMail.style.display = "block";
+    mail.style.border = "solid #FF4E60 2px";
   } else {
-    errorCity.style.display = 'none';
-    radioChecked = true;
-  };
+    errorMail.style.display = "none";
+    mail.style.border = "none";
+    mailChecked = true;
+  }
+
+  if (!birth.value.match(birthdayRegex)) {
+    errorBirth.style.display = "block";
+    birth.style.border = "solid #FF4E60 2px";
+  } else {
+    errorBirth.style.display = "none";
+    birth.style.border = "none";
+    birthChecked = true;
+  }
+
+  if (!tournament.value.match(numbers)) {
+    errorQuantity.style.display = "block";
+    tournament.style.border = "solid #FF4E60 2px";
+  } else {
+    errorQuantity.style.display = "none";
+    tournament.style.border = "none";
+    tournamentChecked = true;
+  }
+
+  radioChecked = false;
+  for (let i = 0; i < locations.length; i++) {
+    if (locations[i].checked) {
+      radioChecked = true;
+    }
+  }
+
+  if (radioChecked) {
+    errorCity.style.display = "none";
+  } else {
+    errorCity.style.display = "block";
+  }
 
   if (!checkbox1CGU.checked) {
-    errorValidation.innerText = 'veuillez accepter les conditions d\'utilisation';
-    errorValidation.style.color = '#FF4E60';
-    errorValidation.style.fontSize = '0.8rem';
-    errorValidation.style.marginTop = '10px';
-    errorValidation.style.marginBottom = '20px';
+    errorValidation.style.display = "block";
+    errorValidation.style.marginBottom = "20px";
   } else {
-    errorValidation.style.display = 'none';
+    errorValidation.style.display = "none";
     CGUChecked = true;
-  };
+  }
 
   // validation final du formulaire
-  if (firstChecked == true && lastChecked == true && mailChecked == true && tournamentChecked == true && radioChecked == true && CGUChecked == true && birthChecked == true) {
+  if (
+    firstChecked &&
+    lastChecked &&
+    mailChecked &&
+    tournamentChecked &&
+    radioChecked &&
+    CGUChecked &&
+    birthChecked
+  ) {
     form.style.display = "none";
     confirmation.style.display = "flex";
   }
 }
-
-
